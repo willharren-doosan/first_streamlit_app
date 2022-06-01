3a6a41a7143dcf585dcf18443958b3fdd199d003
willharren-doosan <65724239+willharren-doosan@users.noreply.github.com>
2022-06-01 11:43:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 #allow user to pick fruit
-streamlit.multiselect("Pick some fruits:" list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 #dispaly the table
 streamlit.dataframe(my_fruit_list)
