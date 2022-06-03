aa0246ff3e78440f4099d7a09d4d8b892204412a
willharren-doosan <65724239+willharren-doosan@users.noreply.github.com>
2022-06-03 14:11:04 -0500

    adding in option to pick fruit and send it back

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,10 @@ streamlit.dataframe(my_fruit_list)
 
 # Lesson 9
 streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user picked', fruit_choice)
 import requests
-fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/"+ fruit_choice)
 
 #make json readable
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
